6147765ff65a757624f71f15f9fc1d94f1234050
EvaM3 <tora.buke@gmail.com>
2023-09-15 21:03:56 +0200

    Initial Commit

---
+++ TriviaGame/TriviaGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGameApp.swift
+//  TriviaGame
+//
+//  Created by Eva Sira Madarasz on 15/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct TriviaGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
